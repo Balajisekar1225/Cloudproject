02ecfb146a6378008723710b15ef2804ba3100d3
VManonmanian <manosir1998@gmail.com>
2023-10-27 09:37:54 +0530

    first comit for balaji

--- src/main/java/com/myapp/CloudProject/CloudProject.java
+++ src/main/java/com/myapp/CloudProject/CloudProject.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CloudProject {
 	@GetMapping (value="admin") 
 		public String getvalue() {
-			return "Welcome to My Api";
+			return "Hi Balaji";
 		}
 	}
 
